711a63fd4bd6ecd5a7cbdaf73d3b42effcdaab45
krilatich <106861958+krilatich@users.noreply.github.com>
2023-11-29 11:42:59 +0700

    init commit

---
+++ app/src/main/java/ru/dima/aston_intensiv_2/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.dima.aston_intensiv_2
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
